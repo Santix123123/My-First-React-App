a7db1061fbac66d3bd491327993b55f6265b629c
Lorenzo Santinelli <lorenzo.santigolf@gmail.com>
2025-03-01 04:07:22 +0100

    Create Greeting.jsx

---
+++ src/Greeting.jsx
@@ -0,0 +1,13 @@
+// Greeting.jsx
+import React from "react";
+
+const Greeting = () => {
+  return (
+    <div>
+      <h1>Hello, Odin Project!</h1>
+      <p>Welcome to my first React component.</p>
+    </div>
+  );
+};
+
+export default Greeting;
